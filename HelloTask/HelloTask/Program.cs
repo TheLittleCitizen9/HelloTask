429457da370eb08ee2f4a0e91dd2206cb75b1247
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-24 21:28:57 +0300

    Added function to print numbers in a syncronic way

---
+++ HelloTask/HelloTask/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void PrintSyncronic()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
