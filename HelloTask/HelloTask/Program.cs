eac605e5b359f6756f41bc369693126f115f4103
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-24 21:53:33 +0300

    Finished exercise 2 a

--- HelloTask/HelloTask/Program.cs
+++ HelloTask/HelloTask/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
 using System.Threading;
 
 namespace HelloTask
 {
     class Program
     {
+        public static Stack<int> _stack= new Stack<int>();
         static void Main(string[] args)
         {
-            PrintThreads();
+            MakeStack();
+            PrintStackValue();
+            Console.ReadLine();
+        }
+
+        public static void MakeStack()
+        {
+            for (int i = 1; i <= 5000; i++)
+            {
+                _stack.Push(i);
+            }
+        }
+
+        public static void PrintStackValue()
+        {
+            for (int i = 1; i <= 3; i++)
+            {
+                int threadNum = i + 1;
+                int result;
+                while(_stack.TryPop(out result))
+                {
+                    Thread thread = new Thread(() => Print(result));
+                    //thread.IsBackground = true;
+                    thread.Start();
+                }
+                   
+            }
         }
 
         public static void PrintSyncronic()
@@ -25,6 +54,17 @@ namespace HelloTask
                 int threadNum = i + 1;
                 Thread thread = new Thread(() => Print(threadNum));
                 thread.Start();
+            }
+        }
+
+        public static void PrintThreadPool()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Console.WriteLine(obj);
+                }, i);
             }
         }
 
